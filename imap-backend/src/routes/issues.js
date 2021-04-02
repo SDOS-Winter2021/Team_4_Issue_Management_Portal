d7a535333c6637393ca53f10a45069b3071aa958
Nandini Bansal <nandini18056@iiitd.ac.in>
2021-04-02 21:55:52 +0530

    Update issues.js

--- imap-backend/src/routes/issues.js
+++ imap-backend/src/routes/issues.js
@@ -17,7 +17,6 @@ router.post("/addIssue", (req, res) => {
   var admin= req.body.administration;
   var public = true;
   var resolved = false;
-  console.log(admin, "aaaaa",typeof admin);
   const issue = new issues({
     
     userEmail: email,

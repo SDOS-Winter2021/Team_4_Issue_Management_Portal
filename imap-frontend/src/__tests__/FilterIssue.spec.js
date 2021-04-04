2e551c21f185847a1fd50d14d3366c436fdac154
dibyaaaaax <dibya.gautam7@gmail.com>
2021-04-04 15:11:28 +0530

    Add tests for Filter and Search Issues

---
+++ imap-frontend/src/__tests__/FilterIssue.spec.js
@@ -0,0 +1,23 @@
+import FilterIssues from "../../src/logics/FilterIssues";
+
+describe("Filter function", () => {
+  test("Returns list of booleans, denoting whether to display the issue after filter", () => {
+    const issues = [
+      { Filter: { Batch: ["2020"], Department: ["CSAM"], Programs: ["PhD"] } },
+      {
+        Filter: { Batch: ["2018"], Department: ["CSAM"], Programs: ["B-Tech"] },
+      },
+      {
+        Filter: { Batch: ["2020"], Programs: ["B-Tech"], Programs: ["B-Tech"] },
+      },
+      { Filter: { Batch: ["2019"], Department: ["CSE"], Programs: ["PhD"] } },
+    ];
+    const filters = [
+      { title: "Batch", filterDetails: [{ title: "2020", isChecked: true }] },
+      { title: "Programs", filterDetails: [{ title: "PhD", isChecked: true }] },
+    ];
+    const output = [true, false, false, false];
+
+    expect(FilterIssues(filters, issues)).toEqual(output);
+  });
+});
